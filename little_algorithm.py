41adc02ac7f9015ff0ab2d7bf0c80148749ef2d3
Arkadiusz Kontek <arek8131@op.pl>
2021-05-27 15:55:36 +0200

    Update little_algorithm.py
    Co-Authored-By: keliasz <61945734+keliasz@users.noreply.github.com>
    Co-Authored-By: dominik-air <62311964+dominik-air@users.noreply.github.com>

--- little_algorithm.py
+++ little_algorithm.py
@@ -14,7 +14,28 @@ def reduction(G):
 
     return G, sum(row) + sum(col)
 
+def get_vertex_cost(G, row, col):
+    min_rows = G[row, :]
+    min_cols = G[:, col]
+
+    min_row = np.inf
+    min_col = np.inf
+
+    for i in range(len(min_rows)):
+        if i != col and min_rows[i] < min_row:
+            min_row = min_rows[i]
+
+        if i != row and min_cols[i] < min_col:
+            min_col = min_cols[i]
+
+    return min_row + min_col
 
 
 if __name__ == '__main__':
-    pass
+    cost_matrix = np.array([[np.inf, 10, 8, 19, 12],
+                      [10, np.inf, 20,  6,  3],
+                      [8,   20, np.inf, 4,  2],
+                      [19,  6,  4, np.inf,  7],
+                      [12,  3,  2,   7, np.inf]])
+
+    print(get_vertex_cost(reduction(cost_matrix)[0], 0, 1))
